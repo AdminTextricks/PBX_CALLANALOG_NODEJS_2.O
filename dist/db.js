ac21f3782f89b46d9b8f86b2836452a8a13f0f49
santanutextrickqqqqqqqqqqqqqqqqqqqqs <director@textricks.com>
2024-09-24 11:31:32 +0530

    updated build with pool connection, balance for reseller

--- dist/db.js
+++ dist/db.js
@@ -9,7 +9,7 @@ const config = {
     user: process.env.DEV_DB_USER,
     password: process.env.DEV_DB_PASSWORD,
     database: process.env.DEV_DB_NAME,
-    timezone: 'Z'
+    timezone: "Z"
   },
   production: {
     host: process.env.PROD_DB_HOST,
@@ -17,7 +17,7 @@ const config = {
     user: process.env.PROD_DB_USER,
     password: process.env.PROD_DB_PASSWORD,
     database: process.env.PROD_DB_NAME,
-    timezone: 'Z'
+    timezone: "Z"
   },
   local: {
     host: process.env.LOCAL_DB_HOST,
@@ -29,26 +29,38 @@ const config = {
 
 // Select the configuration based on the current environment
 const dbConfig = config[env];
-let db;
-function handleDisconnect() {
-  db = mysql.createConnection(dbConfig);
-  db.connect(err => {
-    if (err) {
-      console.error("Error connecting to database:", err);
-      setTimeout(handleDisconnect, 2000); // Retry connection after 2 seconds
-    } else {
-      console.log("Connected to database!");
-    }
-  });
-  db.on('error', err => {
-    console.error("Database error:", err);
-    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-      handleDisconnect(); // Reconnect on connection lost
-    } else {
-      throw err;
-    }
-  });
-}
-handleDisconnect();
+
+// let db;
+
+// function handleDisconnect() {
+//   db = mysql.createConnection(dbConfig);
+
+//   db.connect((err) => {
+//     if (err) {
+//       console.error("Error connecting to database:", err);
+//       setTimeout(handleDisconnect, 2000); // Retry connection after 2 seconds
+//     } else {
+//       console.log("Connected to database!");
+//     }
+//   });
+
+//   db.on('error', (err) => {
+//     console.error("Database error:", err);
+//     if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+//       handleDisconnect(); // Reconnect on connection lost
+//     } else {
+//       throw err;
+//     }
+//   });
+// }
+
+// handleDisconnect();
+
+const db = mysql.createPool(dbConfig);
+
+// Error handling for the pool
+db.on("error", err => {
+  console.error("Database error:", err);
+});
 module.exports = db;
 //# sourceMappingURL=db.js.map
